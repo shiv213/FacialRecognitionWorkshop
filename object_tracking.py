7911fc9d62c1679bdecf858747f8e997918e4c7b
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2021-07-16 15:03:03 -0400

    7/9

--- object_tracking.py
+++ object_tracking.py
@@ -6,11 +6,10 @@ cam = cv.VideoCapture(0)
 while True:
     ret, frame = cam.read()
     frame = imutils.resize(frame, width=600)
-    cv.imshow("video", frame)
 
     # defining boundaries or range for color (in HSV color space)
-    lower = (36, 61, 0)
-    upper = (56, 147, 255)
+    lower = (19, 29, 0)
+    upper = (88, 172, 255)
 
     # blurring image
     blurred = cv.GaussianBlur(frame, (11, 11), 0)
@@ -36,6 +35,18 @@ while True:
         # draw each contour on the output image using a 3px thick red outline, then display the contours
         cv.drawContours(output, [c], -1, (0, 0, 255), 3)
         cv.imshow("contours", output)
+
+    # check if there's at least one contour
+    if len(contours) > 0:
+        largest_contour = max(contours, key=cv.contourArea)     # find largest contour
+        ((x, y), radius) = cv.minEnclosingCircle(largest_contour)
+        M = cv.moments(largest_contour)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / int(M["m00"])))
+
+        cv.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        cv.circle(frame, center, 5, (0, 0, 255), -1)
+
+    cv.imshow("video", frame)
 
     if cv.waitKey(1) == 27:
         break
